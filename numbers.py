099346fba0d7c77fb4b3cefc4025269c54f6c8a4
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-06 11:16:28 +0300

    integer data type

---
+++ numbers.py
@@ -0,0 +1,14 @@
+car_model="BMW"
+car_color="Red"
+car_year=1992
+car_color="Green"
+print(car_model, car_color, car_year)
+print(car_color)
+
+
+age_1=10
+age_2=23
+age_3=67
+
+count_of_age=3
+print(age_1+age_2+age_3//count_of_age)
