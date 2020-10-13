4ba4be35cbec18af186cedb277ce3de07fce2265
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-13 10:29:07 +0300

    tuples

---
+++ dictonaries.py
@@ -0,0 +1,20 @@
+dict_example={
+    'first_name':'Mike',
+    'last_name': 'Ivamov',
+    'age':43,
+    'hobbies': ['football', 'singing', 'photo'],
+    'children': {'son': 'Michael', 'daugter': 'Yana'}
+}
+
+print(dict_example['age'])
+print(dict_example['hobbies'][0])
+
+
+comp_example={
+    'processor':'244mHz',
+    'RAM': 4,
+    'HDD':500,
+    'video':  'ATI Radeon 4455' ,
+    'form_factor': {'power': 660, 'factor': 'ATX'}
+}
+print(comp_example)
---
+++ tuplies.py
@@ -0,0 +1,15 @@
+comp_example=(
+    'processor-244mHz',
+    'RAM 4GB',
+    'HDD 500 GB',
+    'video ATI Radeon 4455' ,
+    'form_factor ATX',
+     500)
+
+print(comp_example)
+
+
+processor, RAM,HDD, video, form_factor, power =comp_example
+
+
+print(processor, RAM,HDD, video, form_factor, power)
