4b996b0c23ed35a91dbe75d8c74feb30aed1686e
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-15 14:00:48 +0300

    booleans task

---
+++ booleans.py
@@ -0,0 +1,15 @@
+first_value=45
+second_value=49
+print(first_value>second_value)
+print(first_value<second_value)
+print(first_value==second_value)
+print(first_value!=second_value)
+
+
+print(ord('A'))
+print(ord('a'))
+
+captial_letter_code=ord('A')
+letter_code=ord('a')
+print(captial_letter_code>letter_code)
+
