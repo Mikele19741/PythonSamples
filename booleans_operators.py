e75037526ea5d0d4062061055082a6f2ec44c67a
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-19 10:39:18 +0300

    conductional operators

---
+++ booleans_operators.py
@@ -0,0 +1,15 @@
+lucky_number=input('Enter any number')
+if lucky_number==int(7):
+    print('7 is a lucky number! Today is your lucky day!')
+else:
+    print( 'Thank you! Have a nice day!')
+
+
+
+x=input('Enter any number')
+
+if int(x) % 2 == 0:
+     print('x is even')
+else:
+     print('x is odd')
+
