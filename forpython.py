593f8d6231e7fab700200c90c5da416ef1e90359
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-20 17:06:22 +0300

    loop for

---
+++ forpython.py
@@ -0,0 +1,8 @@
+number_list = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29 , 30]
+for number in number_list:
+   if number % 2 == 0:
+       print(number)
+lucky_number=input('Enter any number')
+for num in range(int(lucky_number)):
+    print("Hello!")
+
