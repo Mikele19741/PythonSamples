991445d1a7540e199ec32a2eff7e7456f09609a4
Mikhail Shilkov <mshilkov@scnsoft.com>
2020-10-21 11:15:06 +0300

    nested looos

---
+++ nested_loops.py
@@ -0,0 +1,11 @@
+smile='\U0001f600'
+for i in range(10):
+    count=0
+    emoticons=''
+    while count<=i:
+        emoticons+=smile
+        count+=1
+        print(emoticons)
+
+for number in range(10):
+    print(smile*number)
